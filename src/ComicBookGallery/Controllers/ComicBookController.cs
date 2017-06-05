9b7663a9b3ee05056817b4e4455922386f81b140
Eduardo Velez <evelez1990@hotmail.com>
2017-06-04 22:29:35 -0400

    Update detail action method results type

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,5 +8,16 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday )
+            {
+                return Redirect("/");
+            }
+
+             return Content("Hello");
+            
+           // return "Hello";
+        }
     }
 }
