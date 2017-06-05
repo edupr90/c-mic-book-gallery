c4946366be919e5912cb2f5632a7e11cd3641d22
Eduardo Velez <evelez1990@hotmail.com>
2017-06-05 12:00:52 -0400

    Fixed bugs

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -12,7 +12,7 @@ namespace ComicBookGallery.Controllers
         {
 
             ViewBag.SeriesTitle = "The Amazing Spider Man";
-            ViewBag.IissueNumber = 700;
+            ViewBag.IssueNumber = 700;
             ViewBag.Description = "<p>Final issure! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong> will Peter Parker?</strong></p>";
             ViewBag.Artists = new string[]
             {
