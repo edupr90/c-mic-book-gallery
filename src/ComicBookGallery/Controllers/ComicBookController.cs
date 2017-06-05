78674ccb39dc1895a3b57dc97bf22d106c468208
Eduardo Velez <evelez1990@hotmail.com>
2017-06-05 11:47:37 -0400

    Updated comic view detail with view bag

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -11,6 +11,18 @@ namespace ComicBookGallery.Controllers
         public ActionResult Detail()
         {
 
+            ViewBag.SeriesTitle = "The Amazing Spider Man";
+            ViewBag.IissueNumber = 700;
+            ViewBag.Description = "<p>Final issure! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong> will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Elipoulos"
+            };
+
             return View();
 
         }
